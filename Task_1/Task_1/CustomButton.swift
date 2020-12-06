fec913a2acc31a1bcc522e1bdd18edb27f7357dd
Michael <qst77@yandex.ru>
2020-12-06 23:58:31 +0300

    adding hw2_1

---
+++ Task_1/Task_1/CustomButton.swift
@@ -0,0 +1,8 @@
+//
+//  CustomButton.swift
+//  Task_1
+//
+//  Created by Nins on 06.12.2020.
+//
+
+import 
---
+++ Task_1/Task_1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Task_1
+//
+//  Created by Nins on 06.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var RegisterButton: UIButton!
+    @IBOutlet weak var LoginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        LoginButton.layer.cornerRadius = 30
+        RegisterButton.layer.cornerRadius = 30
+        
+        // Do any additional setup after loading the view.
+    }
+
+   
+    
+   
+}
+
